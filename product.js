8496fd741154d75f9d0565a39d3004f7030e9cf8
dalmedian27 <kkla1124@gmail.com>
2023-06-11 20:32:59 -0400

    add product

---
+++ product.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Apple AirPods",
+    price: 129.00,
+    description:
+      "The new AirPods combine intelligent design with breakthrough technology and crystal clear sound. Powered by the new Apple H1 headphone chip, AirPods now feature hands-free access to Siri using just your voice.",
+    image: "/images/product1.jpg",
+  },
+  {
+    id: 2,
+    name: "Bose Sport Earbuds",
+    price: 129.00,
+    description:
+      "Bose Sport Earbuds are true wireless Bluetooth headphones built to produce sound that makes you feel like the performer is right there beside you, pushing you to go one more mile or do one more rep.",
+    image: "/images/product2.jpg",
+  },
+  {
+    id: 3,
+    name: "Beats by Dr. Dre Powerbeats Pro",
+    price: 179.95,
+    description:
+      "Totally wireless Powerbeats Pro earphones are built to revolutionize your workouts. With zero wires to hold you back, the adjustable, secure-fit ear hooks are customizable for extended comfort and stability.",
+    image: "/images/product3.jpg",
+  },
+  {
+    id: 4,
+    name: "Beats Studio3",
+    price: 189.00,
+    description:
+      "Beats Studio3 Wireless headphones deliver a premium listening experience with Pure Adaptive Noise Canceling (Pure ANC) to actively block external noise, and real-time audio calibration to preserve clarity, range, and emotion.",
+    image: "/images/product4.jpg",
+  },
+  {
+    id: 5,
+    name: "Turtle Beach - Stealth 600 Gen 2",
+    price: 99.95,
+    description:
+      "The Turtle Beach Stealth 600 Gen 2 USB Wireless Amplified Video Gaming Headset delivers high-quality audio & chat for PS5, PS4, PS4 Pro, Nintendo Switch, PC & Mac. A long-lasting 15 hour battery means less charging.",
+    image: "/images/product5.jpg",
+  },
+  {
+    id: 6,
+    name: "HyperX Cloud II",
+    price: 99.00,
+    description:
+      "HyperX Cloud II Gaming Headset features a newly designed USB sound card audio control box that amplifies audio and voice for an optimal Hi-Fi gaming experience. Pinpoint your opponent's location in the game and strike before he sees you coming.",
+    image: "/images/product6.jpg",
+  },
+  {
+    id: 7,
+    name: "Microsoft Xbox Wireless Headset",
+    price: 99.99,
+    description:
+      "Game loud and clear with the Xbox Wireless Headset, Auto-mute, and voice isolation reduces background noise and allows for crystal-clear chat.",
+    image: "/images/product7.jpg",
+  },
+  {
+    id: 8,
+    name: "JBL Live 400BT",
+    price: 99.95,
+    description:
+      "Don’t let the music stop! Stay in touch with your surroundings while listening to your tunes. A touch of the Ambient Aware button increases the ambient sound and lets you hear what’s happening around you.",
+    image: "/images/product8.jpg",
+  },
+  {
+    id: 9,
+    name: "Bose QuietComfort Earbuds",
+    price: 279.00,
+    description:
+      "Block out distractions, hear only your music and embrace the moment. Bose QuietComfort Bluetooth Earbuds are made with world-class noise cancelling technology, and put you right in the middle of a live concert without ever leaving your house.",
+    image: "/images/product9.jpg",
+  },
+  {
+    id: 10,
+    name: "Apple AirPods Max",
+    price: 549.00,
+    description:
+      "AirPods Max reimagine over-ear headphones. Industry-leading Active Noise Cancellation blocks outside noise, while Transparency mode lets it in. And spatial audio with dynamic head tracking provides theater-like sound that surrounds you.",
+    image: "/images/product10.jpg",
+  },
+];
