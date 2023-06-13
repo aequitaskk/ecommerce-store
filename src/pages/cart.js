f855101b51f4534fe9a3e75af3b43f425229b2a4
dalmedian27 <kkla1124@gmail.com>
2023-06-12 22:00:00 -0400

    implement checkout-session

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,37 @@
 import CartProduct from "@/components/CartProduct";
+import { useState } from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import axios from "axios";
 
 function CartPage() {
-  const { cartCount, clearCart, formattedTotalPrice, cartDetails } =
-    useShoppingCart();
+  const {
+    cartCount,
+    clearCart,
+    formattedTotalPrice,
+    cartDetails,
+    redirectToCheckout,
+  } = useShoppingCart();
+
+  const [isRedirecting, setIsRedirecting] = useState(false);
+
+  const onCheckout = async () => {
+    if (cartCount > 0) {
+      try {
+        setIsRedirecting(true);
+        const { id } = await axios
+          .post("/api/checkout-sessions", cartDetails)
+          .then((res) => res.data);
+        const result = await redirectToCheckout(id);
+        if (result?.error) {
+          console.log("Error in result: ", result);
+        }
+      } catch (error) {
+        console.log("Error: ", error);
+      } finally {
+        setIsRedirecting(false);
+      }
+    }
+  };
 
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
@@ -11,7 +39,7 @@ function CartPage() {
         <>
           <h2 className="text-4xl font-semibold">Your cart</h2>
           <div className="flex items-center mt-1">
-            <p className="text-xl">{cartCount} items</p>
+            <p className="text-xl ml-2">{cartCount} items</p>
             <button
               onClick={() => clearCart()}
               className="opacity-50 hover:opacity-100 transition text-base ml-1"
@@ -36,8 +64,12 @@ function CartPage() {
               <p className="text-xl">Total:</p>
               <p className="font-semibold text-xl">{formattedTotalPrice}</p>
             </div>
-            <button className="border rounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 transition border-yellow-500 hover:border-yellow-600 focus:ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-yellow-500 mt-4 max-w-max">
-              Checkout
+            <button
+              disabled={isRedirecting}
+              onClick={onCheckout}
+              className="border rounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 transition border-yellow-500 hover:border-yellow-600 focus:ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-yellow-500 mt-4 max-w-max"
+            >
+              {isRedirecting ? "Redirecting..." : "Checkout"}
             </button>
           </div>
         </div>
